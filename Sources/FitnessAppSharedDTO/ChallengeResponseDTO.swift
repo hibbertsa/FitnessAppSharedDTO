e1e80d47c589331364e1942aa19c8c6e6f74051f
Stephen Hibbert <hibbert.stephen@gmail.com>
2023-08-21 19:41:55 -0400

    Modified ChallengeResponseDTO to include error and reason

--- Sources/FitnessAppSharedDTO/ChallengeResponseDTO.swift
+++ Sources/FitnessAppSharedDTO/ChallengeResponseDTO.swift
@@ -9,14 +9,18 @@ import Foundation
 
 public struct ChallengeResponseDTO: Codable {
     
-    public let id: UUID
-    public let adminUserId: UUID
-    public let title: String
-    public let startDate: Date
-    public let endDate: Date
-    public let caloriesPerPoint: Int
+    public let error: Bool
+    public var reason: String?
+    public let id: UUID?
+    public let adminUserId: UUID?
+    public let title: String?
+    public let startDate: Date?
+    public let endDate: Date?
+    public let caloriesPerPoint: Int?
     
-    public init(id: UUID, adminUserId: UUID, title: String, startDate: Date, endDate: Date, caloriesPerPoint: Int) {
+    public init(error: Bool, reason: String?, id: UUID, adminUserId: UUID, title: String, startDate: Date, endDate: Date, caloriesPerPoint: Int) {
+        self.error = error
+        self.reason = reason
         self.id = id
         self.adminUserId = adminUserId
         self.title = title
