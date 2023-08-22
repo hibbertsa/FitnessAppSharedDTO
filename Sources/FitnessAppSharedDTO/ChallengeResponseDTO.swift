d13027d2df0bfc7d0994c4f0e9be0efd7df82c27
Stephen Hibbert <hibbert.stephen@gmail.com>
2023-08-22 00:13:44 -0400

    Added join code to ChallengeResponseDTO

--- Sources/FitnessAppSharedDTO/ChallengeResponseDTO.swift
+++ Sources/FitnessAppSharedDTO/ChallengeResponseDTO.swift
@@ -17,12 +17,13 @@ public struct ChallengeResponseDTO: Codable {
     public var startDate: Date? = nil
     public var endDate: Date? = nil
     public var caloriesPerPoint: Int? = nil
+    public var joinCode: String? = nil
     public var numOfParticipants: Int? = nil
     public var numOfTeams: Int? = nil
     public var dailyPointsEarned: Int? = nil
     public var totalPointsEarned: Int? = nil
     
-    public init(error: Bool, reason: String? = nil, id: UUID? = nil, adminUserId: UUID? = nil, title: String? = nil, startDate: Date? = nil, endDate: Date? = nil, caloriesPerPoint: Int? = nil, numOfParticipants: Int? = nil, numOfTeams: Int? = nil, dailyPointsEarned: Int? = nil, totalPointsEarned: Int? = nil) {
+    public init(error: Bool, reason: String? = nil, id: UUID? = nil, adminUserId: UUID? = nil, title: String? = nil, startDate: Date? = nil, endDate: Date? = nil, caloriesPerPoint: Int? = nil, joinCode: String? = nil, numOfParticipants: Int? = nil, numOfTeams: Int? = nil, dailyPointsEarned: Int? = nil, totalPointsEarned: Int? = nil) {
         self.error = error
         self.reason = reason
         self.id = id
@@ -31,6 +32,7 @@ public struct ChallengeResponseDTO: Codable {
         self.startDate = startDate
         self.endDate = endDate
         self.caloriesPerPoint = caloriesPerPoint
+        self.joinCode = joinCode
         self.numOfParticipants = numOfParticipants
         self.numOfTeams = numOfTeams
         self.dailyPointsEarned = dailyPointsEarned
