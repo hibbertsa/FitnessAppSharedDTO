cf07a27927e543f5cc90a6e7de24dc3eb2debc25
Stephen Hibbert <hibbert.stephen@gmail.com>
2023-08-23 21:51:23 -0400

    Bug fix

--- Sources/FitnessAppSharedDTO/ParticipantResponseDTO.swift
+++ Sources/FitnessAppSharedDTO/ParticipantResponseDTO.swift
@@ -11,10 +11,12 @@ public struct ParticipantResponseDTO: Codable {
     
     public let id: UUID
     public let user: UserResponseDTO
+    public let activityLogs: [ActivityLogResponseDTO]
     
-    public init(id: UUID, user: UserResponseDTO) {
+    public init(id: UUID, user: UserResponseDTO, activityLogs: [ActivityLogResponseDTO]) {
         self.id = id
         self.user = user
+        self.activityLogs = activityLogs
     }
     
 }
