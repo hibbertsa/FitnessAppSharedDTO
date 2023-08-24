ab0c31fddab5fe12b6e3fcd7af1d507cec59fce8
Stephen Hibbert <hibbert.stephen@gmail.com>
2023-08-24 16:59:26 -0400

    Added myDailyPoints and myTotalPoints to Challenge Response DTO

--- Sources/FitnessAppSharedDTO/ChallengeResponseDTO.swift
+++ Sources/FitnessAppSharedDTO/ChallengeResponseDTO.swift
@@ -19,8 +19,10 @@ public struct ChallengeResponseDTO: Codable {
     public let participants: [ParticipantResponseDTO]
     public let teams: [TeamResponseDTO]
     public let scoreboard: [ScorecardResponseDTO]
+    public var myDailyPoints: Int? = nil
+    public var myTotalPoints: Int? = nil
     
-    public init(id: UUID, admin: UserResponseDTO, joinCode: String, title: String, startDate: Date, endDate: Date, caloriesPerPoint: Int, participants: [ParticipantResponseDTO], teams: [TeamResponseDTO], scoreboard: [ScorecardResponseDTO]) {
+    public init(id: UUID, admin: UserResponseDTO, joinCode: String, title: String, startDate: Date, endDate: Date, caloriesPerPoint: Int, participants: [ParticipantResponseDTO], teams: [TeamResponseDTO], scoreboard: [ScorecardResponseDTO], myDailyPoints: Int? = nil, myTotalPoints: Int? = nil) {
         self.id = id
         self.admin = admin
         self.joinCode = joinCode
@@ -31,6 +33,8 @@ public struct ChallengeResponseDTO: Codable {
         self.participants = participants
         self.teams = teams
         self.scoreboard = scoreboard
+        self.myDailyPoints = myDailyPoints
+        self.myTotalPoints = myTotalPoints
     }
     
 }
