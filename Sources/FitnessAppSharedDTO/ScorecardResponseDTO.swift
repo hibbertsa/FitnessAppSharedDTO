3bcaba55e6e31199bb2d0674ac778fbda3bec9ad
Stephen Hibbert <hibbert.stephen@gmail.com>
2023-08-24 23:31:00 -0400

    Bug fix

--- Sources/FitnessAppSharedDTO/ScorecardResponseDTO.swift
+++ Sources/FitnessAppSharedDTO/ScorecardResponseDTO.swift
@@ -9,6 +9,7 @@ import Foundation
 
 public struct ScorecardResponseDTO: Codable {
     
+    public let id = UUID()
     public let username: String
     public let teamTitle: String?
     public let dailyCaloriesBurned: Int
