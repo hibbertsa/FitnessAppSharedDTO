711c8efe0436ff7dfb4dbfbd0129dc8ca469e2b9
Stephen Hibbert <hibbert.stephen@gmail.com>
2023-08-27 19:03:33 -0400

    Bug fix

--- Sources/FitnessAppSharedDTO/ScorecardResponseDTO.swift
+++ Sources/FitnessAppSharedDTO/ScorecardResponseDTO.swift
@@ -10,15 +10,17 @@ import Foundation
 public struct ScorecardResponseDTO: Codable {
     
     public var id = UUID()
-    public let participant: ParticipantResponseDTO
+    public let username: String
+    public let myTeamId: UUID?
     public let dailyCaloriesBurned: Int
     public let dailyPointsEarned: Int
     public let totalCaloriesBurned: Int
     public let totalPointsEarned: Int
     
-    public init(id: UUID = UUID(), participant: ParticipantResponseDTO, dailyCaloriesBurned: Int, dailyPointsEarned: Int, totalCaloriesBurned: Int, totalPointsEarned: Int) {
+    public init(id: UUID = UUID(), username: String, myTeamId: UUID?, dailyCaloriesBurned: Int, dailyPointsEarned: Int, totalCaloriesBurned: Int, totalPointsEarned: Int) {
         self.id = id
-        self.participant = participant
+        self.username = username
+        self.myTeamId = myTeamId
         self.dailyCaloriesBurned = dailyCaloriesBurned
         self.dailyPointsEarned = dailyPointsEarned
         self.totalCaloriesBurned = totalCaloriesBurned
