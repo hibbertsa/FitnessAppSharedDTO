0298369f8960e12a5a99195694fe8d8d8e74f8ce
Stephen Hibbert <hibbert.stephen@gmail.com>
2023-08-27 01:21:14 -0400

    Bug fix

--- Sources/FitnessAppSharedDTO/ScoreboardResponseDTO.swift
+++ Sources/FitnessAppSharedDTO/ScoreboardResponseDTO.swift
@@ -9,14 +9,15 @@ import Foundation
 
 public struct ScoreboardResponseDTO: Codable {
     
-    public let scoreboard: [ScorecardResponseDTO]
+    public let allScorecards: [ScorecardResponseDTO]
     public var myScorecard: ScorecardResponseDTO? = nil
-    public var myTeamScoreboard: [ScorecardResponseDTO]? = nil
+    public var myTeamScorecards: [ScorecardResponseDTO]? = nil
     
-    public init(scoreboard: [ScorecardResponseDTO], myScorecard: ScorecardResponseDTO? = nil, myTeamScoreboard: [ScorecardResponseDTO]? = nil) {
-        self.scoreboard = scoreboard
-        self.myScorecard = myScorecard
-        self.myTeamScoreboard = myTeamScoreboard
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.allScorecards = try container.decode([ScorecardResponseDTO].self, forKey: .allScorecards)
+        self.myScorecard = try container.decodeIfPresent(ScorecardResponseDTO.self, forKey: .myScorecard)
+        self.myTeamScorecards = try container.decodeIfPresent([ScorecardResponseDTO].self, forKey: .myTeamScorecards)
     }
     
 }
